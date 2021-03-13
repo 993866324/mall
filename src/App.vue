faffb405d78e5f001ed3d8a5295613348b061af5
qin980725 <993866324@qq.com>
2021-03-14 01:47:03 +0800

    finish tabbar

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
 
 <script>
 import MainTabBar from "@/components/content/mainTabBar/MainTabBar";
+
 export default {
   name: "App",
   components: {
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,14 +1,39 @@
 <template>
-  <div>
-    <h2>Home.vue</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
   </div>
 </template>
 
 <script>
+import NavBar from "@/components/common/navbar/NavBar";
+import { getHomeMultidata } from "@/network/home";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    NavBar
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: []
+    };
+  },
+  created() {
+    getHomeMultidata().then(res => {
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
+  }
 };
 </script>
 
 <style scoped>
+.home-nav {
+  background-color: var(--color-tint);
+  color: white;
+  font-style: 16px;
+}
 </style>

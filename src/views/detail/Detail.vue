6ff270bf0eb03218316c41be3f8d1591ff3a07c6
qin980725 <993866324@qq.com>
2021-03-16 07:34:58 +0800

    创建详情页

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,19 +1,46 @@
 <template>
   <div id="detail">
-    <h1>{{iid}}</h1>
+    <detail-nav-bar />
+    <detail-swiper :topImages="topImages" />
+    <detail-base-info :goods="goods" />
   </div>
 </template>
 
 <script>
+import DetailNavBar from "./childComps/DetailNavBar";
+import DetailSwiper from "./childComps/DetailSwiper";
+import DetailBaseInfo from "./childComps/DetailBaseInfo";
+import { getDetail, Goods } from "@/network/detail";
 export default {
   name: "Detail",
+  components: {
+    DetailNavBar,
+    DetailSwiper,
+    DetailBaseInfo
+  },
   data() {
     return {
-      iid: null
+      iid: null,
+      topImages: [],
+      goods: {}
     };
   },
   created() {
+    // 保存传入的iid
     this.iid = this.$route.params.iid;
+    /// 通过iid请求详情数据
+    getDetail(this.iid).then(res => {
+      console.log(res);
+      const data = res.result;
+      // 获取轮播图图片
+      this.topImages = data.itemInfo.topImages;
+      // 获取商品信息
+      this.goods = new Goods(
+        data.itemInfo,
+        data.columns,
+        data.shopInfo.services
+      );
+    });
   }
 };
 </script>
